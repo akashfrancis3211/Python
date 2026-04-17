0f288d307060a8556a7079f97d5351ba419f9f7e
Akash Francis <akashfrancis321@gmail.com>
2026-04-17 03:00:15 +0000

    Day 1 of Python

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+name = input("Enter Your Name: ")
+print("Hello," , name + "!, Welcome To Python")
+
+#simple addition example
+a=int(input("Enter First Number: "))
+b=int(input("Enter Second Number: "))
+c=a+b
+print("The Sum of your Nos is ", c)
+
